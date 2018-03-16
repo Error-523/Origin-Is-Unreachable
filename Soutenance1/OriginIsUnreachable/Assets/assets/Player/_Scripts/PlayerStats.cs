d8f85ae951cb2c1e34c0bb19873765fb4d4ad753
Florentin Soubrand <florentin-soubrand@users.noreply.github.com>
2018-03-16 01:38:23 +0100

    Soutenance1
    Build final pour pres
    Menu FPS torch mummy simple ia and so on

---
+++ Soutenance1/OriginIsUnreachable/Assets/assets/Player/_Scripts/PlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerStats : NetworkBehaviour {
+
+    public int healthbase = 3;
+    public int healthax = 3;
+    public static bool isAttack = true;
+    public static bool isDamage = false;
+    
+    public void applyDamage(int theDamage)
+    {
+        if (isLocalPlayer)
+        {
+            healthbase -= theDamage;
+            if (healthbase <= 0)
+            {
+                isAttack = false;
+                Dead();
+            }
+            isDamage = true;
+        }
+    }
+
+    public void Dead()
+    {
+        if (isLocalPlayer)
+        {
+            Debug.Log("player died");
+        }
+    }
+}
---
+++ Soutenance1/OriginIsUnreachable/Assets/assets/Script/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float speed = 0.001f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+
+    }
+
+    void move()
+    {
+        if (Input.GetKey(KeyCode.Z))
+            transform.position += Vector3.forward * speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * speed;
+        if (Input.GetKey(KeyCode.Q))
+            transform.position += Vector3.left * speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * speed;
+    }
+
+}
