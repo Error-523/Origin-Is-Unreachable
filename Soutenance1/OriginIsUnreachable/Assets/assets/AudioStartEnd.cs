0685c73ff56954222c2dac7e814d4ba61b2b22ad
Florentin Soubrand <florentin-soubrand@users.noreply.github.com>
2018-03-30 17:03:41 +0200

    Ajout de l'audio
    + lampe bouge en meme temps que la tete

---
+++ Soutenance1/OriginIsUnreachable/Assets/assets/AudioStartEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioStartEnd : MonoBehaviour {
+
+    public AudioSource effect;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        effect.Play();
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        effect.Stop();
+    }
+}
