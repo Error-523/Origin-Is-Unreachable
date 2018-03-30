b0b7757ec588f044ee9d69fb85c8754a3ee29dcd
Florentin Soubrand <florentin-soubrand@users.noreply.github.com>
2018-03-30 02:08:48 +0200

    Add Directory SetUpCharacter

---
+++ SetUpCharacter/Assets/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PauseMenu : NetworkBehaviour {
+
+    public Transform Player;
+    public Transform canvas;
+
+    private void Update()
+    {if (Input.GetKey(KeyCode.Escape))
+            {
+
+                if (canvas.gameObject.activeInHierarchy == false)
+                {
+                    Player.GetComponent<PlayerControllerSolo>().enabled = false;
+                    canvas.gameObject.SetActive(true);
+                    Cursor.visible = true;
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    Player.GetComponent<PlayerControllerSolo>().enabled = true;
+                    canvas.gameObject.SetActive(false);
+                    Cursor.visible = false;
+                    Time.timeScale = 1;
+                }
+            
+        }
+    }
+}
