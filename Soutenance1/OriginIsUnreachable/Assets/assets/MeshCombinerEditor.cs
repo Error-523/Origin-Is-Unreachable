41803dde477e5aeeed89eab3e636277751c1144b
Florentin Soubrand <florentin-soubrand@users.noreply.github.com>
2018-04-26 15:46:12 +0200

    Classify

---
+++ Soutenance1/OriginIsUnreachable/Assets/assets/MeshCombinerEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CustomEditor (typeof (AdvancedMerge))]
+public class MeshCombinerEditor : Editor {
+
+    
+
+    void OnSceneGUI()
+    {
+        AdvancedMerge am = target as AdvancedMerge;
+        if (Handles.Button(am.transform.position + Vector3.up * 5, Quaternion.LookRotation(Vector3.up), 1, 1, Handles.CubeHandleCap))
+            am.AdvanceMerge();
+    }
+}
