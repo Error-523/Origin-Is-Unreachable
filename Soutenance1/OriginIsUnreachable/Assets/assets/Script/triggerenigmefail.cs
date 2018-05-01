a645f8dd5b3ad2d3c49cf46a540ad7cad5522d7b
Tibal <thibault@github.com>
2018-05-01 22:31:37 +0200

    j

---
+++ Soutenance1/OriginIsUnreachable/Assets/assets/Script/triggerenigmefail.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerenigmefail : MonoBehaviour {
+
+    public bool opening = false;
+    public Transform door;
+    public float speed;
+    public float movement;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (opening)
+        {
+            door.position = new Vector3(door.position.x, door.position.y, door.position.z - movement);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.transform.tag == "Player")
+        {
+            opening = true;
+        }
+    }
+
+}
