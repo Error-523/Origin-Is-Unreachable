b1a3caedbba6b268d242088d65083cb2e1c64995
maxchdn <maxchdn@users.noreply.github.com>
2018-05-03 16:23:06 +0200

    anim

--- Scripts/PlayerController_Net.cs
+++ Scripts/PlayerController_Net.cs
@@ -6,11 +6,15 @@ using UnityEngine.Networking;
 public class PlayerController_Net : NetworkBehaviour {
 
     public float speed;
+    public float runSpeed;
+    private float currentSpeed;
     public float rotateSpeed;
     public float mouseX;
     public float mouseY;
     float sensitivity = 5f;
     public Behaviour[] compToDisable;
+
+    public Rigidbody rigidbody;
 
 
     private void Start() 
@@ -40,17 +44,26 @@ public class PlayerController_Net : NetworkBehaviour {
 
     void FixedUpdate ()
     {
+        if (rigidbody.velocity == Vector3.zero)
+            GetComponent<Animator>().Play("2HIdle");
         if (Camera.main != null)
         {
             Camera.main.gameObject.SetActive(false);
         }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f * speed;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f * speed;
+        if (Input.GetKeyDown(KeyCode.RightShift) || Input.GetKeyDown(KeyCode.LeftShift))
+            currentSpeed = runSpeed;
+        else
+            currentSpeed = speed;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f * currentSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f * currentSpeed;
 
         
         transform.Translate(x, 0, z);
-
+        if (currentSpeed == runSpeed)
+            GetComponent<Animator>().Play("1HCombatRunF");
+        else
+            GetComponent<Animator>().Play("2HWalkF");
         mouseX += Input.GetAxis("Mouse X") * sensitivity;
         transform.rotation = Quaternion.Euler(0, mouseX, 0);
         UpdateCamera();
